4b015fe8d86b6342082cd391e3b76f1bd70df5dc
Redeks Gg <ddredeks@gmail.com>
2025-09-02 19:34:15 +0500

    Task3

--- Tyuiu.ZaicevYaA.Sprint5.Task2.V28.Lib/Class1.cs
+++ Tyuiu.ZaicevYaA.Sprint5.Task2.V28.Lib/Class1.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.ZaicevYaA.Sprint5.Task2.V28.Lib
 {
-    public class Class1
+    public class DataService : ISprint5Task2V28
     {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            // Создаем временный файл
+            string tempFile = Path.GetTempFileName();
 
+            // Обрабатываем матрицу и записываем в файл
+            using (StreamWriter writer = new StreamWriter(tempFile, false, Encoding.Default))
+            {
+                int rows = matrix.GetLength(0);
+                int cols = matrix.GetLength(1);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    string[] lineValues = new string[cols];
+
+                    for (int j = 0; j < cols; j++)
+                    {
+                        // Заменяем положительные элементы на 1, отрицательные на 0
+                        if (matrix[i, j] > 0)
+                            lineValues[j] = "1";
+                        else
+                            lineValues[j] = "0";
+                    }
+
+                    // Записываем строку с разделителем ";"
+                    writer.WriteLine(string.Join(";", lineValues));
+                }
+            }
+
+            return tempFile;
+        }
     }
 }
--- Tyuiu.ZaicevYaA.Sprint5.Task2.V28/Program.cs
+++ Tyuiu.ZaicevYaA.Sprint5.Task2.V28/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Tyuiu.ZaicevYaA.Sprint5.Task2.V28.Lib;
+
+namespace Tyuiu.ZaicevYaA.Sprint5.Task2.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            // Исходная матрица 3x3
+            int[,] matrix = new int[3, 3]
+            {
+                { -5, -2, -5 },
+                { 3, 1, -3 },
+                { 9, -2, -9 }
+            };
+
+            // Вывод исходной матрицы
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{matrix[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            // Создаем экземпляр сервиса и обрабатываем данные
+            DataService ds = new DataService();
+            string outputFile = ds.SaveToFileTextData(matrix);
+
+            // Читаем результат из файла и выводим на консоль
+            string[] resultLines = File.ReadAllLines(outputFile);
+
+            Console.WriteLine("Преобразованная матрица:");
+            foreach (string line in resultLines)
+            {
+                string[] values = line.Split(';');
+                foreach (string value in values)
+                {
+                    Console.Write($"{value,4}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Файл сохранен: {outputFile}");
+
+            // Очистка временного файла (опционально)
+            File.Delete(outputFile);
+
+            Console.ReadKey();
+        }
+    }
+}

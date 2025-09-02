c9fdc1c21507363563d9c74140651ab9bc242b36
Redeks Gg <ddredeks@gmail.com>
2025-09-02 20:42:22 +0500

    44

--- Tyuiu.ZaicevYaA.Sprint5.Task5.V3/Program.cs
+++ Tyuiu.ZaicevYaA.Sprint5.Task5.V3/Program.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.ZaicevYaA.Sprint5.Task5.V3
         static void Main(string[] args)
         {
             // Создаем путь к файлу
-            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V3.txt");
+            string path = Path.Combine(@"C:\DataSprint5", "InPutDataFileTask5V3.txt"); ;
 
             DataService ds = new DataService();
 
